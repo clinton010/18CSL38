1713ec065f8aeb040f636800d68079746672591d
clinton010 <71704992+clinton010@users.noreply.github.com>
2021-02-06 17:10:56 +0530

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,4 +1,81 @@
 #include<stdio.h>
+#include<string.h>
+
+int top, j, i;
+char infix[20], postfix[20], s[20], symbol;
+
+int F(char symbol) {
+  switch (symbol) {
+  case '+':
+  case '-':
+    return 2;
+  case '*':
+  case '/':
+    return 4;
+  case '^':
+  case '$':
+    return 5;
+  case '(':
+    return 0;
+  case '#':
+    return -1;
+  default:
+    return 8;
+  }
+}
+int G(char symbol) {
+  switch (symbol) {
+  case '+':
+  case '-':
+    return 1;
+  case '*':
+  case '/':
+    return 3;
+  case '^':
+  case '$':
+    return 6;
+  case '(':
+    return 9;
+  case ')':
+    return 0;
+  default:
+    return 7;
+  }
+}
+void infix_postfix(char infix[], char postfix[]) {
+  top = -1;
+  s[++top] = '#';
+  j = 0;
+  for (i = 0; i < strlen(infix); i++) {
+    symbol = infix[i];
+    while (F(s[top]) > G(symbol)) {
+      postfix[j] = s[top--];
+      j++;
+    }
+    if (F(s[top]) != G(symbol))
+      s[++top] = symbol;
+    else
+      top--;
+  }
+  while (s[top] != '#') {
+    postfix[j++] = s[top--];
+  }
+  postfix[j] = '\0';
+}
+void main() 
+{
+  printf("\nEnter a valid infix expression\n");
+  scanf("%s", &infix);
+  infix_postfix(infix, postfix);
+  printf("\nThe infix expression is:\n");
+  printf("%s", infix);
+  printf("\nThe postfix expression is:\n");
+  printf("%s", postfix);
+  return 0;
+}
+--------------------------------------------------------------------------------------------------------------
+// method 2
+#include<stdio.h>
 #include<ctype.h>
 char stack[20];
 int top=-1;
